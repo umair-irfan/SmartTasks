ff1e581e1c46ddc2bc87e36662bb5080aa4d7999
Umair <umaironline@live.com>
2025-06-21 12:54:18 +0500

    Configured Scene Delagate + Removed Main Storyboard

--- Smart tasks/SceneDelegate.swift
+++ Smart tasks/SceneDelegate.swift
@@ -13,10 +13,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        //MARK: Custom Entry Point for Application
+        // Navigations are managed through Co-ordinators
+        // Dependecies are Injected in Co-ordinators via DIC
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        
+        //Model View ViewModel
+        
+        //MARK: Dependencies
+        let viewController = ViewController()
+        
+        let rootNavigation = UINavigationController(rootViewController: viewController)
+        window.rootViewController = rootNavigation
+        window.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
